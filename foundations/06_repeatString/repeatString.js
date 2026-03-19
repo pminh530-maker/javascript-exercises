4e194d85049a72dc97d31d204030a10083fa174c
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-03-19 16:22:34 +0700

    Create a loop and add comment to epxplain

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {return "ERROR"};
+    let result = ""; //This empty variable needs to be declared OUTSIDE and BEFORE the loop. Otherwise, the result gets reset every time the loop runs.
+    for (i = 0; i < num; i++) { 
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
