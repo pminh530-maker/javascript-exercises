96f6bfd5df69e506880dcd600b55e3ff13c3c084
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-03-23 11:38:02 +0700

    Improve readability for the math result

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -2,11 +2,11 @@
 const celsius = `${(fahreinheit - 32) * 5/9} °F`; */
 
 const convertToCelsius = function(fahrenheit) {
-  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
+  return Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-  return Math.round (((celsius * 9/5 + 32) *10)) / 10;
+  return Math.round (((celsius * 9) / 5 + 32) *10) / 10;
 };
 
 // Do not edit below this line
