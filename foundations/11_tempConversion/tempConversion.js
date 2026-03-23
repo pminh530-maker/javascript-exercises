3f8254263bebd4aeda5c112eb9eb13125c5349e0
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-03-23 11:35:49 +0700

    Submit a working solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const fahreinheit = `${(celcius * 9/5 + 32)} °C`;
-const celcius = `${(fahreinheit - 32) * 5/9} °F`;
+/* const fahreinheit = `${(celcius * 9/5 + 32)} °C`;
+const celsius = `${(fahreinheit - 32) * 5/9} °F`; */
 
-const convertToCelsius = function() {
-
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round (((celsius * 9/5 + 32) *10)) / 10;
 };
 
 // Do not edit below this line
