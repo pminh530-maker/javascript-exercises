331b62049d5d5fc251f5c2ba0cfde606f41df63f
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-04-08 15:40:28 +0700

    Submit a working solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	for(let i = number; i > 0; i--) {
+    number * i;
+  }
 };
 
 // Do not edit below this line
