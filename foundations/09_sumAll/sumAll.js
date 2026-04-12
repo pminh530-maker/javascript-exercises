7cd26111bf17370a570d49f27757a00a649aa47c
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-04-12 10:15:11 +0700

    Enable all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -3,10 +3,11 @@ const sumAll = function(min, max) {
     if (min < 0 || max < 0) return "ERROR";
 
     if (min > max) [min, max] = [max, min];
+    // array destructuring syntax: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
 
     let sum = 0;
     for (let i = min; i <= max; i++) {
-        sum += i;
+      sum += i;
     }
     return sum;
 };
@@ -23,7 +24,7 @@ This creates a temporary place for min to stay so that min and max do not become
 
 A solution for big numbers (using the Gauss formula)
 
-let numberOfTerms = (max - min) + 1;
+let numberOfTerms = (max - min) + 1; term (Mathematics) = each of the quantities in a ratio, series, or mathematical expression.
 let sum = ((min + max) * numberOfTerms) / 2;
 
 */
